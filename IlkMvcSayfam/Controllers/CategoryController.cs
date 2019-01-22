12a71f9447c1b1b75265598d2b648547aadbe126
Gözde Anagül <gozde.anagul@outlook.com>
2019-01-22 10:24:48 +0300

    ASP baslangıc

---
+++ IlkMvcSayfam/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using IlkMvcSayfam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IlkMvcSayfam.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // get: category
+        public ActionResult Index()
+        {
+            var data = new NorthwindSabahEntities()
+                .Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+            return View(data);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+
+            var data = new NorthwindSabahEntities().Categories.Find(id.Value);
+            if (data == null) RedirectToAction("Index");
+            return View(data);
+        }
+    }
+}
