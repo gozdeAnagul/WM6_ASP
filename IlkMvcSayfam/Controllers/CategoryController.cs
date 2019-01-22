863e9d11341132bd50964008541f14dda67246fe
Gözde Anagül <gozde.anagul@outlook.com>
2019-01-22 10:26:01 +0300

    category controller

--- IlkMvcSayfam/Controllers/CategoryController.cs
+++ IlkMvcSayfam/Controllers/CategoryController.cs
@@ -27,5 +27,51 @@ namespace IlkMvcSayfam.Controllers
             if (data == null) RedirectToAction("Index");
             return View(data);
         }
+        [HttpPost]
+        public ActionResult Add(Category category)
+        {
+            try
+            {
+                var db = new NorthwindSabahEntities();
+                db.Categories.Add(category);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            var db = new NorthwindSabahEntities();
+            try
+            {
+                var category = db.Categories.Find(id.GetValueOrDefault());
+                if (category == null) return RedirectToAction("Index");
+
+                db.Categories.Remove(category);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        public JsonResult Categories()
+        {
+            var categoriler = new NorthwindSabahEntities().Categories.Select(x => new
+            {
+                x.CategoryName,
+                x.CategoryID,
+                x.Description,
+                ProductCount = x.Products.Count
+            }).ToList();
+
+            return Json(categoriler, JsonRequestBehavior.AllowGet);
+        }
     }
 }
