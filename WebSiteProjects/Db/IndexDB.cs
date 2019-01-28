f270a6628becc1da6bf69ce31b23269500cb69f5
Gözde Anagül <gozde.anagul@outlook.com>
2019-01-28 15:05:38 +0300

    chooseList

---
+++ WebSiteProjects/Db/IndexDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSiteProjects.Db
+{
+    public class IndexDB
+    {
+        public IndexDB()
+        {
+
+        }
+        public Dictionary<string,string> ChooseList { get; set; }
+        public static Dictionary<string, string> WhyChooseUs()
+        {
+        }
+    }
+}
