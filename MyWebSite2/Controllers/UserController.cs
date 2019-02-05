64fe4eee3565b27b5de6ee654cac5fdf2ca99513
Gözde Anagül <gozde.anagul@outlook.com>
2019-02-05 14:50:54 +0300

    cmd

--- MyWebSite2/Controllers/UserController.cs
+++ MyWebSite2/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using MyWebSite2.Models;
+﻿using MyWebSite2.BLL;
+using MyWebSite2.DAL.Entities;
+using MyWebSite2.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,11 @@ namespace MyWebSite2.Controllers
 {
     public class UserController : Controller
     {
+        UserService _userService;
+        public UserController()
+        {
+            _userService = new UserService();
+        }
         // GET: User
         public ActionResult RegisterUser()
         {
@@ -18,6 +25,13 @@ namespace MyWebSite2.Controllers
         [HttpPost]
         public ActionResult RegisterUser(UserVM item)
         {
+            _userService.AddUser(new User {
+                Email = item.Email,
+                FirstName = item.FirstName,
+                Gender = item.Gender,
+                LastName = item.LastName,
+                Password = item.Password
+            });
             return View();
         }
     }
