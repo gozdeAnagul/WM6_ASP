1f946f7cf3932ed048786dab85798090f09df2ed
Gözde Anagül <gozde.anagul@outlook.com>
2019-02-27 14:55:09 +0300

    angular js başlangıç

---
+++ WebApiAngularJs/Scripts/app.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module("myApp", []);
+app.controller("ProductCtrl", function ($scope)
+{
+    $scope.urunler = [];
+    $scope.ekle = function ()
+    {
+        $scope.urunler.push({
+            urunAdi: $scope.yeni.urunAdi,
+            fiyat: $scope.yeni.fiyat,
+            eklemeZamani: new Date()
+        })
+    }
+
+
+}
