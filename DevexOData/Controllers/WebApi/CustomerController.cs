29a2887aa7b99298f45300bec10494e528ab91c1
Gözde Anagül <gozde.anagul@outlook.com>
2019-03-05 11:18:52 +0300

    customer entity and fake data Ok

---
+++ DevexOData/Controllers/WebApi/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using DevexOData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace DevexOData.Controllers.WebApi
+{
+    public class CustomerController : ApiController
+    {
+        public IHttpActionResult GetAll()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            return Ok(new
+            {
+                success = true,
+                data = db.Customers.ToList()
+            });
+        }
+    }
+
+}
---
+++ DevexOData/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevexOData.Startup))]
+namespace DevexOData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
